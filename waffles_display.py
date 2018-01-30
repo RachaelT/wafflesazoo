43bf751715ff626925b370a9de005b2c08ec828f
rachael <rachaelst@mac.com>
2018-01-30 15:44:12 -0500

    added to website design

--- waffles_display.py
+++ waffles_display.py
@@ -4,15 +4,19 @@ import time
 import random
 from rx import Observable, Observer
 
-
+#Updates the actual displayed image based on waffle's reported emotional state.
+#Subscribes to a merged stream of emotions from emotion handler, and from being petted
+#TODO: Publish when being petted to stop commands from emotion handler, or have petted command stop stream somehow? 
 class UpdateDisplay(Observer):
-	def on_next( x):
+	def on_next(x):
 		screen.fill((255, 255, 255))
 
+		#Quits when the screen is closed
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				exit(0)
 		
+		#Loads the appropriate image for the emotion
 		if x == "happy":
 			screen.blit(pygame.transform.scale(get_image('faces/waffles_happy.png'), (400, 300)), (0, 0))
 		elif x == "sad":
@@ -25,10 +29,8 @@ class UpdateDisplay(Observer):
 			screen.blit(pygame.transform.scale(get_image('faces/waffles_sleep.png'), (400, 300)), (0, 0))
 		else:
 			screen.blit(pygame.transform.scale(get_image('faces/waffles_neutral.png'), (400, 300)), (0, 0))
-		pygame.display.flip()
 		
-
-
+		pygame.display.flip()
 
 	def on_error(self, error):
 		print("Got error: %s" % error)
@@ -36,8 +38,7 @@ class UpdateDisplay(Observer):
 	def on_completed(self):
 		print("Sequence completed")
 		
-
-
+#Helper method for retrieving images
 _image_library = {}
 def get_image(path):
         global _image_library
@@ -48,20 +49,22 @@ def get_image(path):
                 _image_library[path] = image
         return image
 
-
-
+#Temporary random emotion generator
+#TODO: Convert to "petting" handler, merge with separate observable for emotion stream
 def gen_emote(observer):
 	while True:
-
+		#true when waffles is being petted
 		if(pygame.mouse.get_pressed()[0] == 1):
 				pygame.event.clear()
 				observer.on_next("happy")
+		#Randomly generates emotions
 		else:
 			emotions = ["sad", "playful", "confused", "sleep", ""]
 			index = random.randint(0,4)
 			observer.on_next(emotions[index])
 		clock.tick(30)
 
+#Actually starting the game
 pygame.init()
 screen = pygame.display.set_mode((400, 300))
 done = False
@@ -69,6 +72,7 @@ clock = pygame.time.Clock()
 
 
 emote_gen = Observable.create(gen_emote)
+#emote_and_pet = Observable.merge(check_for_pet, )
 disp = emote_gen.subscribe(UpdateDisplay)
 
 
--- wafflezWebsite.py
+++ wafflezWebsite.py
@@ -1,19 +1,35 @@
 import random
 import os, os.path
-import string
+from cherrypy.lib import static
 
 
 import cherrypy
 
 throttle = 0
 turn = 0
+zephyr = "No recent zephyrs"
+faces = {"happy": 'static/waffles_happy.png', 
+         "confused": 'static/waffles_confused.png',
+         "playful": 'static/waffles_playful.png',
+         "neutral": 'static/waffles_neutral.png',
+         "sad": 'static/waffles_sad.png',
+         "sleep": 'static/waffles_sleep.png'}
+face = 'neutral'
+
+
+def get_image(path):
+        canonicalized_path = path.replace('/', os.sep).replace('\\', os.sep)
+        image = canonicalized_path
+
+        return image
+
 class StringGenerator(object):
-
-
 
     @cherrypy.expose
     def index(self):
-        return open("index.html")
+        global zephyr
+        index = open("index.html").read().format(face=faces[face], z=zephyr)
+        return index
 
     @cherrypy.expose
     @cherrypy.tools.json_out()
@@ -46,13 +62,33 @@ class StringGenerator(object):
         return [throttle, turn]
 
     @cherrypy.expose
+    @cherrypy.tools.json_out()
+    def get_zephyr(self):
+        print("GOTTEN")
+        return zephyr
+
+    @cherrypy.expose
     @cherrypy.tools.json_in()
-    def setState(self):
+    def set_state(self):
         input_json = cherrypy.request.json
         global throttle, turn
         throttle, turn = input_json
 
+    @cherrypy.expose
+    @cherrypy.tools.json_in()
+    def set_zephyr(self):
+        input_json = cherrypy.request.json
+        global zephyr
+        zephyr = input_json
 
+    @cherrypy.expose
+    @cherrypy.tools.json_in()
+    def set_mood(self):
+        
+        input_json = cherrypy.request.json
+        global faces, face
+        face = input_json
+        
 
 
 if __name__ == '__main__':
@@ -68,7 +104,7 @@ if __name__ == '__main__':
         },
         '/static': {
             'tools.staticdir.on': True,
-            'tools.staticdir.dir': './public'
+            'tools.staticdir.dir': 'faces'
         }
     }
     webapp = StringGenerator()
