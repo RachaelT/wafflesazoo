97aecc160172338de2a34163283bb202170e91a0
Rachael S Thompson <rsthomp@mit.edu>
2018-02-03 17:12:15 -0500

    connected nodes, serial communication

--- robot/waffles_display.py
+++ robot/waffles_display.py
@@ -2,11 +2,12 @@ import pygame
 import os
 import time
 import random
+import requests
 from rx import Observable, Observer
+from waffles_feels import waffles_feels
+from waffles_commands import WafflesCMD
 
 #Updates the actual displayed image based on waffle's reported emotional state.
-#Subscribes to a merged stream of emotions from emotion handler, and from being petted
-#TODO: Publish when being petted to stop commands from emotion handler, or have petted command stop stream somehow? 
 class UpdateDisplay(Observer):
 	def on_next(x):
 		screen.fill((255, 255, 255))
@@ -15,24 +16,16 @@ class UpdateDisplay(Observer):
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				exit(0)
+
 		
 		#Loads the appropriate image for the emotion
-		if x == "happy":
-			screen.blit(pygame.transform.scale(get_image('faces/waffles_happy.png'), (400, 300)), (0, 0))
-		elif x == "sad":
-			screen.blit(pygame.transform.scale(get_image('faces/waffles_sad.png'), (400, 300)), (0, 0))
-		elif x == "playful":
-			screen.blit(pygame.transform.scale(get_image('faces/waffles_playful.png'), (400, 300)), (0, 0))
-		elif x == "confused":
-			screen.blit(pygame.transform.scale(get_image('faces/waffles_confused.png'), (400, 300)), (0, 0))
-		elif x == "sleep":
-			screen.blit(pygame.transform.scale(get_image('faces/waffles_sleep.png'), (400, 300)), (0, 0))
-		else:
-			screen.blit(pygame.transform.scale(get_image('faces/waffles_neutral.png'), (400, 300)), (0, 0))
-		
+		screen.blit(pygame.transform.scale(get_image('../faces/waffles_' + x + '.png'), (400, 300)), (0, 0))
+
+		#Updates the website with the new image
+		requests.post("http://wafflesazoo.mit.edu/set_mood", json=x)
 		pygame.display.flip()
 
-	def on_error(self, error):
+	def on_error(error):
 		print("Got error: %s" % error)
 
 	def on_completed(self):
@@ -49,31 +42,26 @@ def get_image(path):
                 _image_library[path] = image
         return image
 
-#Temporary random emotion generator
-#TODO: Convert to "petting" handler, merge with separate observable for emotion stream
-def gen_emote(observer):
-	while True:
-		#true when waffles is being petted
-		if(pygame.mouse.get_pressed()[0] == 1):
-				pygame.event.clear()
-				observer.on_next("happy")
-		#Randomly generates emotions
-		else:
-			emotions = ["sad", "playful", "confused", "sleep", ""]
-			index = random.randint(0,4)
-			observer.on_next(emotions[index])
-		clock.tick(30)
 
-#Actually starting the game
+#Sstarting the display
 pygame.init()
 screen = pygame.display.set_mode((400, 300))
 done = False
 clock = pygame.time.Clock()
 
+CMD = WafflesCMD()
 
-emote_gen = Observable.create(gen_emote)
-#emote_and_pet = Observable.merge(check_for_pet, )
+#Creates an observable that publishes the same stream to multiple observers
+emote_gen = Observable.create(waffles_feels).publish()
+
+#The display subscriber
 disp = emote_gen.subscribe(UpdateDisplay)
+
+#The serial communication subscriber
+cmd = emote_gen.subscribe(CMD)
+
+emote_gen.connect()
+
 
 
 
